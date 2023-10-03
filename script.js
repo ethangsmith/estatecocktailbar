c85de543896b589dd2d0bc76c66b092882ff9fb9
ethangsmith <146752980+ethangsmith@users.noreply.github.com>
2023-10-02 21:34:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// const NavLinkEls = document.querySelectorAll('nav_link');
+// const WindowPathname = window.location.pathname;
+
+// NavLinkEls.forEach(navLinkEl => {
+//     if(navLinkEl.href.includes(WindowPathname)) {
+//         navLinkEl.classList.add('active')
+//     }
+// })
